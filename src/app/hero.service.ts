5b2c975870620cc605a2090f9c4f80145486b37b
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-06-27 16:57:57 +0300

    init Hero-tour

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import {HEROES} from './heroes-mock';
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+}as any)
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes () :Observable<Hero[]> {
+    return of(HEROES)
+  }
+}
