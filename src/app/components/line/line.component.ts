e6ce698e7571fd749f3f0d6f3e256efa675c2eaa
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-07-06 16:30:12 +0300

    fix: draw works in angular way

--- src/app/components/line/line.component.ts
+++ src/app/components/line/line.component.ts
@@ -1,29 +1,30 @@
-import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Point} from '../../interfaces/point-interface';
 import {Scales} from '../../interfaces/scales-interface';
 import {line} from 'd3-shape';
-import {select} from 'd3-selection';
 
 @Component({
   selector: '[app-line]',
   templateUrl: './line.component.html',
   styleUrls: ['./line.component.css']
 })
-export class LineComponent implements OnInit, OnChanges {
+export class LineComponent implements OnInit{
   @Input() line: Point[];
   @Input() scales: Scales;
   @ViewChild('line') linePath: ElementRef;
-  lineFunction: Function = line()
+  private lineFunction: Function = line()
     .x((point) => this.scales.x(point["seconds"]))
     .y((point) => this.scales.y(point['val']));
   constructor() {
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
+  getColor() {
+    console.log(this.line);
+    return this.line[0].color;
   }
-
-  ngOnChanges(changes: SimpleChanges) {
-    select(this.linePath.nativeElement).style("stroke", this.line[0].color).attr("d", this.lineFunction(this.line as any))
+  getData() {
+    return this.lineFunction(this.line as any)
   }
 }
